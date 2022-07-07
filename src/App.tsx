d8fae0f0dc09d4438d5c52e2063043a214f55e1c
chen21414 <chen21414@msn.com>
2022-07-07 00:18:16 -0700

    070722

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList';
 
 function App() {
+
+  // example:
+  // const [number, setNumber] = useState<number | string | boolean>(5);
+
+  // const changeNumber = () => {
+  //   setNumber(10);
+  // }
+
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: 'LeBron James',
+  //     url:'',
+  //     age: 36,
+  //     note: 'Allegeric to staying on the same team'
+  //   },
+  //   {
+  //     name: 'Kobe Bryant',
+  //     url:'',
+  //     age: 36
+  //   }
+  // ]);
+
+   interface IState {
+    people: {
+      name: string
+      age: number
+      url: string
+      note?: string 
+      // ? means optional
+    }[]//means array of object
+  }
+
+  // const [people, setPeople] = useState<{age:number, name: string}[]>([
+      
+  // ]);
+
+  const [people, setPeople] = useState<IState["people"]>([
+      {
+        name: "LeBron James",
+        url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+        age: 36,
+        note: 'Allegeric to staying on the same team'
+      }
+
+  ]);
+
+  // people.map(person => {
+  //   person.name
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>People Invited to my Party</h1>
+        <List people={people}/>
+        <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
